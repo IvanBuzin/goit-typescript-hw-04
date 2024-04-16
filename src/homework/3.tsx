c99b34614000f61b93a8afd3be64f34c3dca0bed
IvanBuzin <ua.buzin@gmail.com>
2024-04-16 10:04:29 +0300

    sdf

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event) => {
     setValue(event.target.value);
   };
 
